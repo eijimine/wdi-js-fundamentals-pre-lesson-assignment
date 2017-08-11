02bf17263fedcc0f41e38e536cfc37b35b7bb5f1
eiji <eijimine@gmail.com>
2017-08-11 00:22:10 -0400

    Assignment Complete

---
+++ pacman-objects-1.js
@@ -0,0 +1,13 @@
+var inky = {};
+inky.colour = 'Cyan';
+inky.personality = 'Shadow';
+console.log(inky);
+
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' };
+console.log(ghosts);
+
+console.log(ghosts.inky);
+console.log(ghosts.blinky);
+
+var ghostName = 'clyde';
+ghosts[ghostName];
